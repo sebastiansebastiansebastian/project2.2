51377f59aab5e1bff473f07e39c88a60b34ebce2
[sfholleufer] <[sebastian@holleufer.dk]>
2016-09-30 13:03:30 +0200

    1.2 - More comments
    Added comments to database.php

--- database.php
+++ database.php
@@ -4,6 +4,9 @@ $username = 'root';
 $password = '';
 $database = 'cph';
 
+/* Connecting to database using PDO (new standard for transfering information)
+ If there's an error, it is caught (catch) and, using a variable, gets echoed out, so you can see it */
+
 try{
 	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 } catch(PDOException $e){
